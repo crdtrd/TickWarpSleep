9d89bf8415b6b47c436f1043691350db040f5bed
crdtrd <crdtrd@gmail.com>
2025-08-09 10:30:18 -0700

    8.9.2025.1020 Opening branch for player sleeping message fix

--- src/main/java/com/drtdrc/tws/mixin/ServerWorldMixin.java
+++ src/main/java/com/drtdrc/tws/mixin/ServerWorldMixin.java
@@ -3,6 +3,7 @@ package com.drtdrc.tws.mixin;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.server.world.SleepManager;
+import net.minecraft.world.MutableWorldProperties;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -20,21 +21,31 @@ public abstract class ServerWorldMixin {
 	@Unique float sleepGameTickRate = 200;
 
 	@Shadow public abstract TickManager getTickManager();
-	@Shadow @Final private List<ServerPlayerEntity> players;
+	@Shadow @Final List<ServerPlayerEntity> players;
 
+	@Shadow protected abstract void wakeSleepingPlayers();
+	@Shadow abstract public void setTimeOfDay(long timeOfDay);
+
+	// @Shadow abstract public boolean isDay();
+
+	// This effectively replaces vanilla sleep logic
+	// Sleeping no longer skips weather events. You can still sleep during thunderstorms.
 	@Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/SleepManager;canSkipNight(I)Z"), method = "tick")
 	private boolean onIsEnoughPlayersSleeping(SleepManager instance, int percentage) {
 		boolean isEnoughResters = instance.canSkipNight(percentage);
 		boolean isEnoughSleepers = instance.canResetTime(percentage, players);
-
+		// TODO add condition for time to wake players
 		if (isEnoughResters && isEnoughSleepers) {
 			if (!warping) {
 				warping = true;
 				this.getTickManager().setTickRate(sleepGameTickRate);
 			}
 		} else {
-			if (warping) {
+			if (warping) { // && isDay()
 				warping = false;
+//				long l = this.properties.getTimeOfDay() + 24000L;
+//				this.setTimeOfDay(l - l % 24000L);
+				this.wakeSleepingPlayers();
 				this.getTickManager().setTickRate(20f);
 			}
 		}
